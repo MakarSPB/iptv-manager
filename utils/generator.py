47beed84d85b37825659bee1d8dedd6ba92d9c4c
Makar-aka <makar@yeby.ru>
2025-12-20 19:25:54 +0300

    #

--- utils/generator.py
+++ utils/generator.py
@@ -2,15 +2,15 @@ def generate_m3u(channels) -> str:
     lines = ["#EXTM3U"]
     for ch in channels:
         extinf = f'#EXTINF:-1'
-        if ch.tvg_id:
-            extinf += f' tvg-id="{ch.tvg_id}"'
-        if ch.tvg_name:
-            extinf += f' tvg-name="{ch.tvg_name}"'
-        if ch.tvg_logo:
-            extinf += f' tvg-logo="{ch.tvg_logo}"'
-        if ch.group_title:
-            extinf += f' group-title="{ch.group_title}"'
-        extinf += f', {ch.name}'
+        if ch.get("tvg_id"):
+            extinf += f' tvg-id="{ch["tvg_id"]}"'
+        if ch.get("tvg_name"):
+            extinf += f' tvg-name="{ch["tvg_name"]}"'
+        if ch.get("tvg_logo"):
+            extinf += f' tvg-logo="{ch["tvg_logo"]}"'
+        if ch.get("group_title"):
+            extinf += f' group-title="{ch["group_title"]}"'
+        extinf += f', {ch["name"]}'
         lines.append(extinf)
-        lines.append(ch.url)
+        lines.append(ch["url"])
     return "\n".join(lines)
